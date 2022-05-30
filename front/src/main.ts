e06b98194598cd302fa87623ce77a3144ae79955
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-30 15:10:32 +0200

    refactor: ok

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,26 +1,35 @@
 import "./style.css";
 
 import { cx0, cy0, r0 } from "./Constants";
+import { Point } from "./interfaces/Point";
 
 console.log("coucou");
 
+const getAngle = (index: number, total: number) => {
+  return (index * (2 * Math.PI)) / total;
+};
+
+const getCoordinatesOnCircle = (angle: number): Point => {
+  return {
+    x: cx0 + r0 * Math.cos(angle),
+    y: cy0 + r0 * Math.sin(angle),
+  };
+};
+
 const drawCircles = (n: number) => {
-  // recuperer l'elt svg g.circles
   const elt = document.querySelector("svg g.circles");
 
   // boucle de 1 a n
   for (let i = 0; i < n; i++) {
-    const angle = (i * (2 * Math.PI)) / n;
-
-    const cx = cx0 + r0 * Math.cos(angle);
-    const cy = cy0 + r0 * Math.sin(angle);
+    const angle = getAngle(i, n);
+    const c = getCoordinatesOnCircle(angle);
 
     const circle = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "circle"
     );
-    circle.setAttributeNS(null, "cx", cx + "");
-    circle.setAttributeNS(null, "cy", cy + "");
+    circle.setAttributeNS(null, "cx", c.x + "");
+    circle.setAttributeNS(null, "cy", c.y + "");
     circle.setAttributeNS(null, "r", "1");
     // ajouter le petit cercle au DOM
     elt.appendChild(circle);
