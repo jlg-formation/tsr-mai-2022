d70a394f551ee2cc82ade61660a135d8cd5a3727
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-30 17:05:54 +0200

    dev: initConfig

--- front/src/Command.ts
+++ front/src/Command.ts
@@ -1,9 +1,26 @@
 import { Config } from "./Config";
 
 export class Command {
-  constructor(private config: Config) {}
+  constructor(private config: Config) {
+    this.applyConfig();
+  }
+
+  applyConfig() {
+    this.setInput("sampleNbr");
+    this.setInput("multiplicationFactor");
+  }
 
   onUpdate(callback: (newConfig: Config) => void) {
     console.log("onUpdate");
+  }
+
+  setInput(key: keyof Config) {
+    // recuperer le text html (la ou il y a le nombre)
+    const elt = document.querySelector(`div.command label.${key} span`);
+    elt.innerHTML = this.config[key] + "";
+    const input = document.querySelector(
+      `div.command label.${key} input`
+    ) as HTMLInputElement;
+    input.value = this.config[key] + "";
   }
 }
--- front/src/main.ts
+++ front/src/main.ts
@@ -3,17 +3,15 @@ import "./style.css";
 import { VisualMultiplicator } from "./VisualMultiplicator";
 
 const main = () => {
-  const visualMultiplicator = new VisualMultiplicator();
-  visualMultiplicator.setConfig({
-    sampleNbr: 34,
-    multiplicationFactor: 12,
-  });
-  visualMultiplicator.draw();
-
-  const command = new Command({
+  const initialConfig = {
     sampleNbr: 10,
     multiplicationFactor: 2,
-  });
+  };
+  const visualMultiplicator = new VisualMultiplicator();
+  visualMultiplicator.setConfig(initialConfig);
+  visualMultiplicator.draw();
+
+  const command = new Command(initialConfig);
   command.onUpdate((config) => {
     visualMultiplicator.setConfig(config);
     visualMultiplicator.draw();
