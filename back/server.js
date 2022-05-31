649cf5dfb409360274b8e877ca65548ae199c6b7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-31 11:28:03 +0200

    dev: commonjs

--- back/server.js
+++ back/server.js
@@ -1,1 +1,12 @@
 console.log("About to start a server");
+
+const express = require("express");
+
+const truc = require("./truc");
+console.log("truc: ", truc);
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server started on port " + 3000);
+});
---
+++ back/truc.js
@@ -0,0 +1,5 @@
+console.log("started truc");
+
+module.exports = {
+  toto: 123,
+};
